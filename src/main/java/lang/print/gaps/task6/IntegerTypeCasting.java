aec3160fb4bdd60b9be71679545b94bd338aacee
Lapa <lapabig@gmail.com>
2023-05-15 14:21:38 +0600

    Updated

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
-        double first = 1000;
+        int first = 1000;
         int second = (int) first;
         System.out.println(first);
         System.out.println(second);
